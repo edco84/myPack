340d55b20178000253d3731604c53fa07ea84c4b
ed <ed@Eduardos-MacBook-Pro.local>
2016-10-05 17:31:29 -0400

    Version 0.0.0.2 Improvement to function

--- R/Functions.R
+++ R/Functions.R
@@ -1,15 +1,23 @@
 #' Document function hi
 #'
 #' Function helps to break the ice.
-#' 
+#'
 #' @param who character(1) The name of the person you wish to start a conversation
-#' 
-#' @return character(1) 
-#' 
-#' @examples 
+#'
+#' @return character(1)
+#'
+#' @examples
 #' hi('your_name')
-#' 
+#'
 #' @export
-#' 
-hi <- function(who)
+#'
+hi <- function(who){
+    stopifnot(is.character(who),
+                length(who)==1,
+              !is.na(who),
+              !is.null(who))
     paste("Hello World!", who, "you have ", nchar(who), "letters in your name.")
+}
+
+shout <- function(who)
+    toupper(who)
